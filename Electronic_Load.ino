60318ffed566911d4099b001fd72cd4c0cf20298
ProgramistaPrzemyslaw <przemyslaw.zeranski@gmail.com>
2022-07-18 16:26:59 +0200

    Added basic encoder and OLED test

--- Electronic_Load.ino
+++ Electronic_Load.ino
@@ -1,9 +1,69 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <splash.h>
+#include "AiEsp32RotaryEncoder.h"
+
+#if defined(ESP8266)
+#define ROTARY_ENCODER_A_PIN D6
+#define ROTARY_ENCODER_B_PIN D5
+#define ROTARY_ENCODER_BUTTON_PIN D7
+#else
+#define ROTARY_ENCODER_A_PIN 32
+#define ROTARY_ENCODER_B_PIN 5
+#define ROTARY_ENCODER_BUTTON_PIN 25
+#endif
+#define ROTARY_ENCODER_STEPS 4
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64 
 
 
-void setup(){
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, -1, ROTARY_ENCODER_STEPS);
+
+void IRAM_ATTR readEncoderISR()
+{
+    rotaryEncoder.readEncoder_ISR();
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    rotaryEncoder.begin();
+    rotaryEncoder.setup(readEncoderISR);
+    rotaryEncoder.setBoundaries(0, 1000, false); //minValue, maxValue, circleValues true|false (when max go to min and vice versa)
+    rotaryEncoder.setAcceleration(250);
+
+      if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("Hello, world!");
+  display.display(); 
 
 }
 
-void loop(){
-    
+void loop()
+{
+    if (rotaryEncoder.encoderChanged())
+    {   
+        display.clearDisplay();
+        Serial.println(rotaryEncoder.readEncoder());
+        display.setCursor(0, 10);
+        display.println(rotaryEncoder.readEncoder());
+        display.display(); 
+    }
+    if (rotaryEncoder.isEncoderButtonClicked())
+    {
+        Serial.println("button pressed");
+    }
 }
