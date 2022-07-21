b6aa69c54bd4c286009138202a81d3d512f39fc3
ProgramistaPrzemyslaw <przemyslaw.zeranski@gmail.com>
2022-07-21 12:27:16 +0200

    Started work on Constant Current Menu

---
+++ CCMenu.cpp
@@ -0,0 +1,57 @@
+#include "menu.h"
+
+
+CCMenu::CCMenu(){
+addLabel(1,"Set Current");
+addLabel(2,"Set Time");
+addLabel(3,"Start");
+addLabel(4,"Back");
+pointerPos=0;
+}
+
+void CCMenu::beginDraw(Adafruit_SSD1306 &display){
+display.clearDisplay();
+}
+
+void CCMenu::drawMenu(Adafruit_SSD1306 &display){
+
+
+display.setTextSize(1);
+display.setTextColor(WHITE);
+
+for(int i=0;i<labels_.size();i++){
+display.setCursor(10, i*10);
+display.println(labels_[i]->getText().c_str());
+}
+
+}
+ 
+
+void CCMenu::addLabel(int8_t number, std::string text){
+labels_.push_back(std::make_shared <LABEL>(number,text));
+}
+
+
+void CCMenu::createMenu(){
+
+}
+
+void CCMenu::drawPointer(int8_t position, Adafruit_SSD1306 &display){
+display.drawRect(0, (position%labels_.size())*10, 10, 10, WHITE);
+}
+
+uint8_t CCMenu::returnLabelCount(){
+    return labels_.size();
+}
+
+void CCMenu::endDraw(Adafruit_SSD1306 &display){
+display.display();
+}
+
+int8_t CCMenu::select(int8_t position){
+return labels_[position]->getNumber();
+}
+
+CCMenu::~CCMenu(){
+    
+}
--- menu.h
+++ menu.h
@@ -18,6 +18,7 @@ public:
 LABEL(int8_t number, std::string text);
 void setText(std::string string);
 std::string getText();
+int8_t getNumber();
 void setNumber(int8_t number);
 ~LABEL();
 };
@@ -33,6 +34,7 @@ virtual void addLabel(int8_t number, std::string text)=0;
 virtual void drawPointer(int8_t position, Adafruit_SSD1306 &display)=0;
 virtual uint8_t returnLabelCount()=0;
 virtual void endDraw(Adafruit_SSD1306 &display)=0;
+virtual int8_t select(int8_t position)=0;
 virtual ~Menu(){};
 };
 
@@ -50,6 +52,24 @@ void addLabel(int8_t number, std::string text);
 void drawPointer(int8_t position, Adafruit_SSD1306 &display);
 uint8_t returnLabelCount();
 void endDraw(Adafruit_SSD1306 &display);
-
+int8_t select(int8_t position);
 ~mainMenu();
+};
+
+class CCMenu: public Menu{
+private:
+std::vector<std::shared_ptr<LABEL>> labels_;
+uint8_t pointerPos;
+
+public:
+CCMenu();
+void beginDraw(Adafruit_SSD1306 &display);
+void drawMenu(Adafruit_SSD1306 &display);
+void createMenu();
+void addLabel(int8_t number, std::string text);
+void drawPointer(int8_t position, Adafruit_SSD1306 &display);
+uint8_t returnLabelCount();
+void endDraw(Adafruit_SSD1306 &display);
+int8_t select(int8_t position);
+~CCMenu();
 };
